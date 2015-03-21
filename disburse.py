618cb9012faf47d1a174df7e4744c651eff891db
Richard B Winters <rik@mmogp.com>
2015-03-20 20:23:43 -0400

    Project Update
    - Initial commit
    - Includes README.md modified from nk-xrm project
    - Includes LICENSE from nk-xrm project
    - Package installs nk-xrm into the proper directory structure at the
    application root rather than in ./node_modules/
    - Always installs the latest version of nk-xrm for the end user, yet
    will not overwrite an existing installation so as not to destroy
    end-user contributed files
    
    Change-Id: Iec21a6db213644698c7b92ccd78124c838f1e177
    Signed-off-by: Richard B Winters <rik@mmogp.com>

---
+++ disburse.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# __author__ = "Richard B Winters"
+# __copyright__ = "Copyright 2011-2015, Massively Modified, Inc."
+# __credits__ = ["Richard B Winters"]
+# __license__ = "Apache V2.0"
+# __maintainer__ = "Richard B Winters"
+# __email__ = "support@mmogp.com"
+# __status__ = "Production"
+
+import sys
+import os
+import shutil
+import errno
+
+# Prepare paths & variables
+source = os.path.abspath(sys.argv[1])
+destination = os.path.abspath(sys.argv[2])
+ignorables = sys.argv[3]
+
+# Get a list of all source files and directires to copy
+files = os.listdir( source )
+
+# Copy source files to the package destination
+for copyable in files:
+    if copyable in ignorables:
+        print('Ignorable not copied: %s' % copyable)
+    else:
+        copied = destination + "/" + copyable
+        copyable = source + "/" + copyable
+        if os.path.exists(copied):
+            print('Skipping directories/files which already exist: %s' % copied)
+        else:
+            try:
+                shutil.move(copyable, destination)
+            except OSError as e:
+                # The only error we should get is that destination already exists
+                print('Move error: %s' % e.strerror)
+
+print('')
+print('If attempting to update source, please backup and remove the current source at:')
+print('%s (exclude the node_modules directory)' % destination)
+print('')
+
+# Exit
+sys.exit(0)
+
---
+++ unpack.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# __author__ = "Richard B Winters"
+# __copyright__ = "Copyright 2011-2015, Massively Modified, Inc."
+# __credits__ = ["Richard B Winters"]
+# __license__ = "Apache V2.0"
+# __maintainer__ = "Richard B Winters"
+# __email__ = "support@mmogp.com"
+# __status__ = "Production"
+
+import sys
+import os
+import urllib
+import tarfile
+
+# Define remote url and local destination of source archive
+remote = sys.argv[1] + sys.argv[2] + sys.argv[4]
+archive = os.path.abspath(sys.argv[5] + sys.argv[3] + sys.argv[4])
+
+# Fetch source archive
+urllib.urlretrieve (remote, archive)
+
+# Define output directory for decompressed source archive
+destination = os.path.abspath(sys.argv[6])
+
+# Extract
+source = tarfile.open(archive,'r:gz');
+source.extractall(destination)
+
+# Relocate the source to the application rood (i.e to the directory above containing node_modules directory)
+
+
+# Exit
+sys.exit(0)
